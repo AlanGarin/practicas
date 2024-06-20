94c88324f6898a8503df9d354e65675f9af61f1b
Alan Garin <162273987+AlanGarin@users.noreply.github.com>
2024-06-20 17:40:22 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const prompt = require("prompt-sync")()
+
+let mensajeDeBienvenida = 'Bienvenido a la aplicación';
+console.log(mensajeDeBienvenida);
+
+let nombre = "Alan";
+let edad  = 25;
+let numeroDeVentas = 50; 
+let saldo = 1000;
+let mensajeError = '¡error! Completa todos los campos';
+
+nombre = prompt('ingresa tu nombre:');
+edad =  prompt("ingresa tu edad:");
+
+
+if (nombre, edad ===''){
+  console.log(mensajeError)
+}
+else{
+
+  if (edad >= 18){
+    console.log("puedes obtener tu licencia de conducir");
+  }
+  else{
+    console.log("no puedes obtener tu licencia de conducir")
+  }
+  
+}
+
+  
